19f19e7c4002ed4dc0c26613a3e3a8626331f4b7
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-03-12 14:17:45 -0500

    Adding a Mallu App Project and work: buildColumnAtTop: Add the name of each product, buildColumnWithRow: Add the scroll to display the images and prices, Home Page: Here is all the design

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String val = "hai";
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -32,6 +33,7 @@ class HomePage extends StatelessWidget {
                 "Shop ",
                 style: TextStyle(
                   fontSize: 32,
+                  letterSpacing: 1,
                 ),
               ),
               Text(
@@ -39,6 +41,7 @@ class HomePage extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 32,
                   fontWeight: FontWeight.bold,
+                  letterSpacing: 1,
                 ),
               ),
             ],
@@ -58,9 +61,7 @@ class HomePage extends StatelessWidget {
             child: Container(
               decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40)
-                ),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(40)),
                 boxShadow: [
                   BoxShadow(
                     color: Colors.grey,
@@ -69,10 +70,107 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buildColumnAtTop("Candels", isSelected: true),
+                      buildColumnAtTop("Vases"),
+                      buildColumnAtTop("Bins"),
+                      buildColumnAtTop("Floral"),
+                      buildColumnAtTop("Decor"),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        buildColumnWithRow("1", "Elemental Tin Candel", "31"),
+                        buildColumnWithRow("2", "Summer Candel", "25"),
+                        buildColumnWithRow("3", "Winter Candel", "43"),
+                        buildColumnWithRow("4", "Dummy Candel", "65"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
       ),
+    );
+  }
+
+  Padding buildColumnWithRow(String img, String title, String price) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          Container(
+            height: 200,
+            width: 160,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                "assets/images/candel$img.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            "\$ $price",
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Column buildColumnAtTop(String text, {bool isSelected = false}) {
+    return Column(
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            color: isSelected ? Colors.black : Colors.grey,
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        if (isSelected)
+          Container(
+            height: 5,
+            width: 5,
+            decoration: const BoxDecoration(
+              color: Colors.black,
+              shape: BoxShape.circle,
+            ),
+          ),
+      ],
     );
   }
 
